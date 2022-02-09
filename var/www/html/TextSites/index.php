be10119d5c1de259085b22eff0e807fbcf4388f6
DESKTOP-N22G64F\Administrator <r17512846921@163.com>
2022-02-09 17:08:47 +0800

    none

--- var/www/html/TextSites/index.php
+++ var/www/html/TextSites/index.php
@@ -7,10 +7,14 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Images | TESTING</title>
+        <title>Texts | TESTING</title>
         <link rel="stylesheet" type="text/css" href="/css/theme.css">
         <style>
-
+            body
+            {
+                background-color: #222222;
+                color: #ffffff;
+            }
         </style>
     </head>
     <body>
@@ -22,20 +26,13 @@
             <a class="backNormal" href="/index.html">BACK TO MAINPAGE</a>
             <hr />
         </div>
-
-        <h1>Images</h1>
+        <h1>Texts</h1>
 
         <div class="navigationContain">
             <hr />
-            <a class="navigation" href="Manga/index.php">Manga</a>
+            <a class="navigation" href="Novel/index.php">Novel</a>
             <hr />
-            <a class="navigation">ImageSet ( Virtual )</a>
-            <hr />
-            <a class="navigation">ImageSet ( Real )</a>
-            <hr />
-            <a class="navigation">Miscellaneous ( Virtual )</a>
-            <hr />
-            <a class="navigation">Miscellaneous ( Real )</a>
+            <a class="navigation">Reserve</a>
             <hr />
         </div>
     </body>
