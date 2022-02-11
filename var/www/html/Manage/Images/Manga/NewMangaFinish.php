1dee529a9f37736fcfab3533c9d9a9ae45aa9488
DESKTOP-N22G64F\Administrator <r17512846921@163.com>
2022-02-11 23:00:26 +0800

    none

--- var/www/html/Manage/Images/Manga/NewMangaFinish.php
+++ var/www/html/Manage/Images/Manga/NewMangaFinish.php
@@ -3,44 +3,21 @@
     <head>
         <meta charset='utf-8'>
         <title>Verify</title>
+        <link rel="stylesheet" type="text/css" href="/css/theme.css">
         <style>
             body
             {
                 font-size: 30px;
-                font-family: fantasy;
                 background-color: #222222;
                 color: #ffffff;
                 margin: 20px auto;
                 text-align: center;
             }
 
-            a{
-                text-decoration: none;
-            }
-            a:link
-            {
-                color: #ffffff;
-            }
-            a:visited
-            {
-                color: #ffffff;
-            }
-            a:hover
-            {
-                color: #888888;
-            }
-
-            h1
-            {
-                color: orangered;
-                text-align: center;
-                font-size: 50px;
-            }
-
         </style>
     </head>
     <body>
-        <a href="/index.html">BACK TO MAINPAGE</a>
+        <a href="/index.php">BACK TO MAINPAGE</a>
         <hr width="90%" color="orangered"/>
 
         <?php
@@ -69,6 +46,11 @@
             @$cover = fopen("/var/www/html/Images/Manga/Temp/cover.jpg",'r');
 
             $mangaImagePath = "/var/www/html/Images/Manga/$temp[1]";
+            if($temp[1]=='')
+            {
+                echo "ERROR: EMPTY PATH";
+                exit;
+            }
             DELETEDIR($mangaImagePath);
             mkdir($mangaImagePath);
             chmod($mangaImagePath, 0777);
